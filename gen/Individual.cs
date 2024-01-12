9bced0639ee6ce6943a3e8d9ca8d47d3e6c0ffe1
elenadj7 <eelenadjurdjevic@gmail.com>
2024-01-12 23:01:12 +0100

    feat: individual

---
+++ gen/Individual.cs
@@ -0,0 +1,71 @@
+﻿namespace genetic_algorithm.gen
+{
+    public class Individual
+    {
+        private readonly static int Length = 10; // (3 - (-3)) / 2 ^ 10 < 0.01
+        private readonly static int MinX = -3;
+        private readonly static int MaxX = 3;
+        private readonly static int Range = (int)Math.Pow(2, Length); //1024
+
+        private readonly List<int> chromosomeX;
+        private readonly List<int> chromosomeY;
+        private readonly double phenotypeX; // [-3,3]
+        private readonly double phenotypeY; // [-3,3]
+        private readonly double fitness;
+
+        private static List<int> CreateChromosome()
+        {
+            List<int> chromosome = new();
+            Random random = new Random();
+            for(int i = 0; i < Length; i++)
+            {
+                if(random.Next(2) == 0)
+                {
+                    chromosome.Add(0);
+                }
+                else
+                {
+                    chromosome.Add(1);
+                }
+            }
+
+            return chromosome;
+        }
+
+        private static int MapListToInt(List<int> chromosome)
+        {
+            string binaryArray = string.Join("", chromosome);
+            return Convert.ToInt32(binaryArray, 2) % Range;
+        }
+
+        private static double CalcPhenotype(List<int> chromosome)
+        {
+            double min = 0d;
+            double max = (double)Range;
+            int value = MapListToInt(chromosome);
+            return MinX + (MaxX - MinX) * ((value - min) / (max - min));
+        }
+
+        private double CalcFitness()
+        {
+            double result = 1.25d * Math.Pow(1 - phenotypeX, 2) * Math.Pow(Math.E, -Math.Pow(phenotypeX, 2) - Math.Pow(phenotypeY + 1, 2)) 
+                - 10 * (phenotypeX - Math.Pow(phenotypeX, 5) - Math.Pow(phenotypeY, 5)) * Math.Pow(Math.E, -(Math.Pow(phenotypeX, 2) + Math.Pow(phenotypeY, 2)/0.9d))
+                -0.2d * Math.Pow(Math.E, -(Math.Pow(phenotypeX + 1, 2) - Math.Pow(phenotypeY, 2)));
+            return result;
+        }
+
+        public Individual()
+        {
+            chromosomeX = CreateChromosome();
+            chromosomeY = CreateChromosome();
+            phenotypeX = CalcPhenotype(chromosomeX);
+            phenotypeY = CalcPhenotype(chromosomeY);
+            fitness = CalcFitness();
+        }
+
+        public double GetFitness()
+        {
+            return fitness;
+        }
+    }
+}
---
+++ gen/Population.cs
@@ -0,0 +1,42 @@
+﻿namespace genetic_algorithm.gen
+{
+    public class Population
+    {
+        private readonly static double MaxError = 6.251637526363513d;
+
+        //private readonly int size;
+        private List<Individual> individuals = new();
+
+        private int mutation = 0;
+        private int recombination = 0;
+        private double meanSquareError;
+        private double avgFitness;
+        private Individual bestIndividual;
+
+        /*private List<Individual> CreatePopulation()
+        {
+            List<Individual> population = new();
+            for(int i = 0; i < size; i++)
+            {
+                population.Add(new Individual());
+            }
+
+            return population;
+        }*/
+
+        private double CalcAvgFitness()
+        {
+            return CalcTotalFitness() / individuals.Count;
+        }
+
+        private double CalcTotalFitness()
+        {
+            return individuals.Select(i => i.GetFitness()).Sum();
+        }
+
+        private double CalcMeanSquareError()
+        {
+            return individuals.Select(i => Math.Pow(i.GetFitness(), 2)).Average();
+        }
+    }
+}
